7718c8329bfc8ee65702523ca529cad3f4b90cdc
ShitalKarad <shitalkarad24@gmail.com>
2023-03-22 22:16:51 +0530

    Added:Day13 uc2 print array of element

--- employeeWage.js
+++ employeeWage.js
@@ -1,3 +1,4 @@
+console.log("Welcome to employee wage calculation")
 function random(min,max){
 return Math.floor(
     Math.random() * (max - min + 1) + min
@@ -6,10 +7,15 @@ return Math.floor(
 
 let wage = random(0,1);
 console.log("random number is "+wage);
+const WAGE_PER_HOUR = 20;
+const FULL_DAY_HOUR = 8;
 
 if(wage == 1){
-    console.log("Employee is present " );
+    var dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+    console.log("Employee is present & daily wage is " +dailyWage );
 }else{
-    console.log("Employee is  " );
+    var dailyWage = WAGE_PER_HOUR * 0;
+    console.log("Employee is & daily wage is "+ dailyWage );
 }
+
 
