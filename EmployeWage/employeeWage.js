f5f1e574c5ea153215b1a656d245f93f6513777a
ShitalKarad <shitalkarad24@gmail.com>
2023-03-23 22:55:34 +0530

    Added: Total wage for moth

--- EmployeWage/employeeWage.js
+++ EmployeWage/employeeWage.js
@@ -8,15 +8,20 @@ const PART_TIME_HOUR = 4;
 const IS_FULL_TIME = 2;
 const IS_PART_TIME = 1;
 const IS_UPSENT = 0;
+const WORKING_DAYS_PER_MONTH = 22;
 
 switch (wage) {
     case 1:
         var dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-        console.log(" Part time employee is present & daily wage is " + dailyWage);
+        console.log(" Part time employee is present & daily wage is ", dailyWage);
+        var totalWage = dailyWage * WORKING_DAYS_PER_MONTH;
+        console.log("Total wage for a month ", totalWage)
         break;
     case 2:
         var dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-        console.log("Employee is present & daily wage is " + dailyWage);
+        console.log("Employee is present & daily wage is ", dailyWage);
+        var totalWage = dailyWage * WORKING_DAYS_PER_MONTH;
+        console.log("Total wage for a month ", totalWage)
         break;
     default:
         console.log("Employee is upsent")
